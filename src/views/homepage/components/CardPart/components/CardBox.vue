d86b2738f04e3c28aaee330266f0806f4906f871
y19941115mx <1960892068@qq.com>
2022-08-23 09:31:36 +0800

    完成首页框架

---
+++ src/views/homepage/components/CardPart/components/CardBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box h-full">
+    <div v-for="i in [...Array(8).keys()]" :key="i" class="item">
+      <n-space>
+        <span>1</span>
+        <n-avatar round size="small" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+        <n-space :vertical="true">
+          <span>name</span>
+          <span>desc</span>
+        </n-space>
+      </n-space>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.box {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: space-between;
+}
+</style>
