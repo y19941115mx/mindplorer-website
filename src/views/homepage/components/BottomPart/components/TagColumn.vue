22b04806a6916098b279559cf72b94009fe3e425
y19941115mx <1960892068@qq.com>
2022-08-23 09:25:20 +0800

    测试

---
+++ src/views/homepage/components/BottomPart/components/TagColumn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="box">
+    <span class="title">标题</span>
+    <n-space :vertical="true">
+      <span v-for="i in [...Array(5).keys()]" :key="i" class="tag">标签</span>
+    </n-space>
+    <span class="footer">更多</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.title {
+  width: 82px;
+  height: 29px;
+  font-size: 16px;
+  font-family: Microsoft YaHei, Microsoft YaHei-Normal;
+  font-weight: normal;
+  text-align: LEFT;
+  color: rgba(44, 44, 44, 0.85);
+  line-height: 22px;
+}
+</style>
